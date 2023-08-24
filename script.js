fbb09f1130f6d1e276e2fd8568b3ef93bb24c344
Leone Varando <106971202+Opaleone@users.noreply.github.com>
2023-08-24 09:17:01 -0500

    Began work on timer functionality; not completed.

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const main = document.querySelector('#main');
 const card = document.querySelector('.card');
+const timer = document.querySelectorAll('.timer-clicker')
+const timerLength = document.querySelectorAll('.timer-length')
 
-const handleCardClick = () =>  {
-  alert('HELLO')
+// console.log(parseInt(timerLength[0].innerHTML))
+
+
+const handleTimerClick = (time) => {
+  let timeInMs = time
+  
+  const future = new Date().valueOf() + timeInMs
+  const now = new Date().valueOf();
+  const diff = future - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const mins = Math.floor(diff / (1000 * 60));
+  const secs = Math.floor(diff / 1000);
+
+  const h = hours - days * 24;
+  const m = mins - hours * 60;
+  const s = secs - mins * 60;
+
+  console.log(`${h}h ${m}m ${s}s`)
+
+  // console.log()
 }
 
-card.addEventListener('click', handleCardClick);
+// handleTimerClick(parseInt(timerLength[i].innerHTML))
+
+
+
+for (let i = 0; i < timer.length; i++) {
+  timer[i].addEventListener('click', function() {
+    setInterval(function () {
+      let timeInMs = parseInt(timerLength[i].innerHTML) * 60000;
+
+      handleTimerClick(timeInMs)
+
+      timeInMs -= 1000
+    }, 1000)
+  })
+}
